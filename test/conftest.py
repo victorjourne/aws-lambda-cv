a7d2f4a407be865a0642ab5b4f342eb47478b5f7
vjourne <victor.journe@eleves.ec-nantes.fr>
2020-02-18 14:47:09 +0100

    Binary version is working on lambda

--- test/conftest.py
+++ test/conftest.py
@@ -1,28 +1,18 @@
 import pytest
 import base64
-from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 path_cat = 'test/cat.jpg'
 @pytest.fixture
 def apigateway_event():
 
-    mp_encoder = MultipartEncoder(
-        fields={'image': ('filename',open(path_cat, "rb"),'image/png'),
-                'other' : 'babar'
-                })
-    body = mp_encoder.to_string()
-    print('form-data is :')
-    print(body[:100])
-    body = base64.b64encode(body)
-    print('It is encoded in base64')
-
     def _apigateway_event(route):
-        event = dict(httpMethod = 'POST',
-                     path = '/%s'%route,
-                     headers = {'content-type': mp_encoder.content_type},
-                     body = body)
+        with open(path_cat, "rb") as image_file:
+            body = base64.b64encode(image_file.read())
+            event = dict(httpMethod = 'POST',
+                       path = '/%s'%route,
+                       headers = {'Content-Type': 'image/jpg'},
+                       body = body)
+            return event
 
-
-        return event
 
     return _apigateway_event
--- test/test_lambda_handler.py
+++ test/test_lambda_handler.py
@@ -2,7 +2,6 @@ import sys, os
 import pytest
 import io
 
-from requests_toolbelt.multipart.encoder import MultipartEncoder
 # Import lambda_function
 import imp
 #lambda_function = importlib.import_module('.src', package='aws-lambda-cv')
@@ -17,10 +16,10 @@ def test_handler(apigateway_event):
     body = resp['body']
     print(body)
     assert resp['statusCode'] == 200
-    assert eval(body)== [[600, 400, 3]], "shape of image is : %s"% eval(body)
+    assert eval(body)== [600, 400, 3], "shape of image is : %s"% eval(body)
 
     resp = lambda_handler(apigateway_event('nbytes'), None)
     body = resp['body']
     print(body)
     assert resp['statusCode'] == 200
-    assert eval(body)== [720], "size of of image is : %sk bytes "% eval(body)
+    assert eval(body)== 720, "size of of image is : %sk bytes "% eval(body)
